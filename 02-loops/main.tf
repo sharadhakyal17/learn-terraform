128235bb1ce9ac308277412f8dfe15793601525c
shpatil <sharad.hakyal@gmail.com>
2025-03-03 00:08:00 +0530

    loop statement

---
+++ 02-loops/main.tf
@@ -0,0 +1,20 @@
+resource "null_resource" "dummy" {
+  count = length(var.x)
+
+  provisioner "local-exec" {
+    command = "echo ${var.x[count.index]}"
+  }
+
+}
+
+
+variable "x" {
+
+    default =   [
+        1,
+        200,
+        300,
+        400,
+        500
+    ]
+}
