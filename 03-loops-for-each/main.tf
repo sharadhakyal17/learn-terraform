550e1c0ea45b9b9a90fd8174cbd3200aa49ce9cc
shpatil <sharad.hakyal@gmail.com>
2025-03-03 00:16:18 +0530

    for each loop condition

---
+++ 03-loops-for-each/main.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "test" {
+    for_each = var.x 
+    provisioner "local-exec" {
+        command = "echo value of ${each.key} = ${each.value}"
+    }
+}
+
+
+variable "x"  {
+    default = {
+        i = 100
+        j = 200
+    }
+}
